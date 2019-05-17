0133829cece812dcc28ac184caf0cecfd339cb27
Arlo Erdaka <arlo.erdaka@gmail.com>
2019-05-17 14:26:06 +0700

    Initial Commit

---
+++ AnimateHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimateHandsOn
+//
+//  Created by Arlo Erdaka on 17/05/19.
+//  Copyright © 2019 Arlo Erdaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
